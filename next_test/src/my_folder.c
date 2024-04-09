e93d45b96964654399175a8e3b1b71a27cb15e62
Andrey <162986060+AndreyPLtn@users.noreply.github.com>
2024-04-09 16:37:10 +0300

    Update my_folder.c
    1 up

--- next_test/src/my_folder.c
+++ next_test/src/my_folder.c
@@ -11,36 +11,38 @@ int mf_get_count_track(const mf_info_t *) {
 	}
 	return get_count_track; 
 }
-/*
-Информация по файловой системе
-*/
+
 int mf_get_info(int id, mf_info_t* info) {
-	struct stat info_struct {
-		stat("path to file", &info_struct);
-		for(int i = 0; i <=MF_MAX_TRACKS; i++)
-		info->mf_info_t.id_coll= _folders[i].size;
-		info->mf_info_t.id_fold = _folders[i].last_modified;
-		info->mf_info_t.id_track = _folders[i].id;
+	struct stat info_struct;
+		if (stat("path to file", &info_struct) == 0) {
+			for(int i = 0; i < MF_MAX_TRACKS; i++) {
+				if (_folders[i].id == id) {
+					info->id_coll = _folders[i].size;
+					info->id_fold = _folders[i].last_modified;
+					info->id_track = _folders[i].id;
+					return 1; //успешно заполнено
+			}
+		}
 	}
-	return 0;
+	return 0;//track не найден - возвращаем 0
 }
 
 int mf_add(mf_info_t* mf_info) {
-	_folders[MF_MAX_TRACKS-1].id_coll=mf_info->size;
-	_folders[MF_MAX_TRACKS-1].id_fold=mf_info->last_modified;
-	_folders[MF_MAX_TRACKS-1].id_track=mf_info->info;
-	return 0;
+	_folders[MF_MAX_TRACKS-1].id_track = mf_info -> id_track;
+	return 1;//при успешном добавлении возвращается 1
 }
 
-int mf_delete(int id) {
-	for(int i=0; i<MF_MAX_TRACKS; i++){
-		if(_folders[i].id_fold==id){
-			_folders[i]=_folders[MF_MAX_TRACKS-1];
-		break;
+int mf_delete(mf_info_t *info, int id_track) {
+	for(int i = 0; i < MF_MAX_TRACKS; i++) {
+		if (info[i].id_track == id_track) {
+			info[i] = info[MF_MAX_TRACKS - 1];
+            info[MF_MAX_TRACKS - 1].id_track = 0; // Устанавливаем маркер удаления
+            return 1; //успешное удаление
 		}
 	}
-	return 0;
+	return 0; //элемент не найден
 }
+
 void mf_reset() {
 	return;
 }
