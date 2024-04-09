bc5d016242c0d2ac12ff96c5024a1fe81da6a29f
Andrey <162986060+AndreyPLtn@users.noreply.github.com>
2024-04-09 16:56:55 +0300

    Update my_folder.c
    2

--- next_test/src/my_folder.c
+++ next_test/src/my_folder.c
@@ -2,7 +2,7 @@
 
 static mf_info_t _folders[MF_MAX_TRACKS] = {0};
 
-int mf_get_count_track(const mf_info_t *) {
+int mf_get_count_track(const mf_info_t *info) {
 	int get_count_track = 0;
 	for(int i = 0; i < MF_MAX_TRACKS; i++) {
 		if(_folders[i].id_fold != 0) {
@@ -20,29 +20,34 @@ int mf_get_info(int id, mf_info_t* info) {
 					info->id_coll = _folders[i].size;
 					info->id_fold = _folders[i].last_modified;
 					info->id_track = _folders[i].id;
-					return 1; //успешно заполнено
+					return 1; // успешно заполнено
 			}
 		}
 	}
-	return 0;//track не найден - возвращаем 0
+	return 0; //track не найден - возвращаем 0
 }
 
 int mf_add(mf_info_t* mf_info) {
-	_folders[MF_MAX_TRACKS-1].id_track = mf_info -> id_track;
-	return 1;//при успешном добавлении возвращается 1
+    if (num_tracks < MF_MAX_TRACKS) {
+        _folders[num_tracks] = *mf_info;
+        num_tracks++;
+        return 1; // если успешно добавлено
+    } else {
+        return 0; // если достигнуто максимальное количество треков
+    }
 }
 
-int mf_delete(mf_info_t *info, int id_track) {
-	for(int i = 0; i < MF_MAX_TRACKS; i++) {
-		if (info[i].id_track == id_track) {
-			info[i] = info[MF_MAX_TRACKS - 1];
-            info[MF_MAX_TRACKS - 1].id_track = 0; // Устанавливаем маркер удаления
-            return 1; //успешное удаление
-		}
-	}
-	return 0; //элемент не найден
+int mf_delete(int id_track) {
+    for (int i = 0; i < num_tracks; i++) {
+        if (_folders[i].id == id_track) {
+            for (int j = i; j < num_tracks - 1; j++) {
+                _folders[j] = _folders[j + 1];
+            }
+            num_tracks--;
+            return 1; // успешно удалено
+        }
+    }
+    return 0; // трек с указанным id не найден
 }
 
-void mf_reset() {
-	return;
-}
+void mf_reset() {}
