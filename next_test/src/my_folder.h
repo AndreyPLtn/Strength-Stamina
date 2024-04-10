7054fb7601553746f9f5413d8de9b56d4fa51c17
Andrey <162986060+AndreyPLtn@users.noreply.github.com>
2024-04-10 18:48:21 +0300

    Update my_folder.h

--- next_test/src/my_folder.h
+++ next_test/src/my_folder.h
@@ -3,23 +3,35 @@
 #include <stdbool.h> 
 
 #define MF_MAX_TRACKS 10  // максимальное количество элементов (криков) в своей папке
-mf_info_t _folders[MF_MAX_TRACKS];
 
+mf_info_t _folders[MF_MAX_TRACKS];
 int num_tracks = 0; // текущее количество записей криков
 
 // структура элемента (крика) Своей папки
-typedef struct {
+typedef struct { 
 	int id_coll;   // ид фонотеки
 	int id_fold;   // ид папки
-	int id_track;  // ид крика
+	int id_track; // ид крика
+	int data; //ид данных
 } mf_info_t;
+
+ // динамический массив с элементами mf_info_t
+typedef struct {
+    mf_info_t* data; // Указатель на данные
+    int size;        // Текущий размер
+    int capacity;    // Вместимость
+} dynamic_array;
+
+void init_array(dynamic_array* arr) { // инициализирует динамический массив dynamic_array
+	
+};
 
 /**
  * @brief получить количество криков в Своей папке
  *
  * @return int кол-во, -1 - файлов нет
  */
-int mf_get_count_track();
+int mf_get_count_track(const mf_info_t *info);
 
 /**
  * @brief заполнить структуру элемента (крика) Своей папки
@@ -28,7 +40,7 @@ int mf_get_count_track();
  * @param info структура элемента (крика)
  * @return int 0 - заполнить структуру удалось, -1 - ошибка
  */
-int mf_get_info(int id, mf_info_t *info);
+int mf_get_info(int id, mf_info_t* info);
 
 /**
  * @brief добавить элемент (крик) в Свою папку
@@ -36,7 +48,7 @@ int mf_get_info(int id, mf_info_t *info);
  * @param mf_info структура элемента (крика)
  * @return int 0 - успех, -1 - ошибка
  */
-int mf_add(mf_info_t *mf_info);
+int mf_add(dynamic_array* arr, mf_info_t element);
 
 /**
  * @brief удалить элемент (крик) из Своей папки
@@ -44,10 +56,10 @@ int mf_add(mf_info_t *mf_info);
  * @param id номер элемента (крика)
  * @return int int 0 - успех, -1 - ошибка
  */
-int mf_delete(int id);
+int mf_delete(dynamic_array* arr, int index);
 
 /**
  * @brief сброс Своей папки (удаление всех элементов (криков))
  *
  */
-void mf_reset();
+void mf_reset(mf_info_t* _folders, int id_track);
