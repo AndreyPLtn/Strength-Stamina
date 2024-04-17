84266c9ee62a2905870cd3ee597de8efb4dce39e
Andrey <162986060+AndreyPLtn@users.noreply.github.com>
2024-04-17 16:08:22 +0300

    Update main.c

--- test/main.c
+++ test/main.c
@@ -3,16 +3,18 @@
 #include "my_folder.h"
 
 int main() {
-	printf("start app");
-	
-    
-    mf_info_t track_1 = {1, 2, 3};
-    mf_add(&track_1);
+    printf("start appn");
 
-    mf_info_t track_2;
-    mf_get_info(0, &track_2);
-    
+    // проверка
+    mf_info_t track1 = {1, 2, 3};
+    mf_add(&track1);
+
+    mf_info_t track2;
+    mf_get_info(0, &track2);
+
     mf_delete(0);
+
+    mf_reset();
 
     return 0;
 }
