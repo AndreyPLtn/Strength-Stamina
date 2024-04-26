53530dfe0d95205f4c920ee16bf1a411c29e0c88
Andrey <162986060+AndreyPLtn@users.noreply.github.com>
2024-04-26 17:20:46 +0300

    Update my_folder.c

--- test/my_folder.c
+++ test/my_folder.c
@@ -10,38 +10,38 @@ int mf_get_count_track() {
 
 int mf_get_info(int id, mf_info_t* info) {
     if (id < 0 || id >= num_tracks) { // проверка id в диапазоне
-        printf("Invalid id"); // при не соответствии выводит ошибку
-        return -1; // возвращает -1
+        printf("Invalid id");
+        return -1;
     }
 
     *info = _folders[id]; // при совпадении копирует id из _folders в info
-    return 0; // возвращает 0 при успехе
+    return 0;
 }
 
 int mf_add(mf_info_t* mf_info) { // добавление нового крика 
-    if (num_tracks == MF_MAX_TRACKS) { // проверка на максимальное значение криков
+    if (num_tracks == MF_MAX_TRACKS) {
         printf("Error: Not enough free space");
-        return -1; // выводит -1 при отсутствии места
+        return -1;
     }
 
     _folders[num_tracks] = *mf_info; // информация о крике копируется в _folders[] с индексом num_tracks через указатель mf_info 
-    num_tracks++; // увеличение счетчика на 1 после копирования
+    num_tracks++;
 
-    return num_tracks - 1; // возврат присвоенного индекса
+    return num_tracks - 1;
 }
 
 int mf_delete(int id) { // удаление крика по id
-    if (id < 0 || id >= num_tracks) { // проверка в диапазоне 0 - количество криков
+    if (id < 0 || id >= num_tracks) {
         printf("Invalid id");
-        return -1; // выводит -1 при ошибке
+        return -1;
     }
 
     for (int i = id; i < num_tracks - 1; i++) {
         _folders[i] = _folders[i + 1]; // сдвиг 
     }
-    num_tracks--; // -1 у количества крика (свидетельство успешного удаления)
+    num_tracks--;
 
-    return 0; // возврат 0 при успешном удалении
+    return 0;
 }
 
 void mf_reset() { 
