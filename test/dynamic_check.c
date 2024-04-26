de346a0e5ecb1bf54cac4b21482d4b90793e4bfc
Andrey <162986060+AndreyPLtn@users.noreply.github.com>
2024-04-26 17:19:57 +0300

    Update dynamic_check.c

--- test/dynamic_check.c
+++ test/dynamic_check.c
@@ -22,57 +22,57 @@ int mf_get_count_track() {
 }
 
 int mf_get_info(int id, mf_info_t* info) {
-    if (id < 0 || id >= num_tracks) { // проверка id в диапазоне
-        printf("Invalid id"); // при не соответствии выводит ошибку
-        return -1; // возвращает -1
+    if (id < 0 || id >= num_tracks) {
+        printf("Invalid id");
+        return -1;
     }
 
-    *info = _folders[id]; // при совпадении копирует id из _folders в info
-    return 0; // возвращает 0 при успехе
+    *info = _folders[id];
+    return 0;
 }
 
 int mf_add(mf_info_t* mf_info) { // добавление нового крика 
-    if (num_tracks == MF_MAX_TRACKS) { // проверка на максимальное значение криков
+    if (num_tracks == MF_MAX_TRACKS) {
         printf("Error: Not enough free space");
-        return -1; // выводит -1 при отсутствии места
+        return -1;
     }
 
     mf_info_t* temp = (mf_info_t*)realloc(_folders, (num_tracks + 1) * sizeof(mf_info_t)); // изменяет размер выделенной памяти для _folders
     if (temp == NULL) {
         printf("Memory reallocation error");
-        return -1; // выводит -1 при переполнении
+        return -1;
     }
 
     _folders = temp; // присваивает временную переменную в _folders (свидетельство успешного выделения памяти)
-    _folders[num_tracks] = *mf_info; // помещает информацию mf_info в последний элемент _folders[]
-    num_tracks++; // увеличение счетчика на 1
+    _folders[num_tracks] = *mf_info;
+    num_tracks++;
 
-    return num_tracks - 1; // возврат index нового элемента
+    return num_tracks - 1;
 }
 
 int mf_delete(int id) { // удаляет трек из _folders с освобождением памяти
-    if (id < 0 || id >= num_tracks) { // проверка id в указанном диапазоне
-        printf("Invalid id"); // вывод ошибки
-        return -1; // возвращает -1
+    if (id < 0 || id >= num_tracks) {
+        printf("Invalid id");
+        return -1;
     }
 
     for (int i = id; i < num_tracks - 1; i++) { // проверка id через i
-        _folders[i] = _folders[i + 1]; // если id корректный 
+        _folders[i] = _folders[i + 1];
     }
-    num_tracks--; // уменьшает кол-во треков
+    num_tracks--;
 
     _folders = (mf_info_t*)realloc(_folders, num_tracks * sizeof(mf_info_t)); // уменьшает количество выделенной памяти для _folders
-        if (_folders == NULL) { // проверка _folders на равенство NULL
-            printf("Delete error"); // выводит ошибку
-            return -1; // возвращает -1
+        if (_folders == NULL) {
+            printf("Delete error");
+            return -1;
     }
-    return 0; // возврат 0 при успешном удалении
+    return 0;
 }
 
 void mf_reset() {
     free(_folders); // Освобождаем память через функцию free
-    _folders = NULL; // присваивание NULL (_folders не указывает на память)
-    num_tracks = 0; // присваивает num_track нулевое значение
+    _folders = NULL;
+    num_tracks = 0;
 }
 
 int main() {
